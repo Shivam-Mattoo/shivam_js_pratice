661ee80f86a1822b728b9d0c71e83a94f7e79f99
Shivam-Mattoo <shivammattoo69@gmail.com>
2025-09-22 14:15:32 +0000

    stack and heap

---
+++ .devcontainer/01basics/stackandheap.js
@@ -0,0 +1,15 @@
+
+let userone = { 
+    email : "shivammattoo@gmail.com" , 
+    phone : 7534252728 ,
+    name : "shivam"
+}
+
+let usertwo = userone
+
+usertwo.email = "mattoo@google.com"
+//here kyuki ye ek primitive data type tha isliye idhar isme two se change karte hi one mai bhi change hogaya
+console.log( userone)
+
+// lecture 7 ( string in javascript)
+
